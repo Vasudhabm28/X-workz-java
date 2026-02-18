f673c6f983f3dff8e4c19dbdd3e1612148c6d05b
Vasudhabm28 <vasudham282@gmail.com>
2026-02-18 11:00:16 +0530

    examples of Multiplereturntypes

---
+++ CellPhoneExecutor.java
@@ -0,0 +1,8 @@
+class CellPhoneExecutor{
+	public static void main(String[] args){
+		String contactName = "vasudha";
+		long contactNo=CellPhone.getContactNumber(contactName);
+		System.out.println("The contactNo of "+contactName+" is "+contactNo);
+	}
+}
+		
---
+++ ZomatoExecutor.java
@@ -0,0 +1,9 @@
+class ZomatoExecutor {
+    public static void main(String[] args){
+        String foodName = "pizza";
+        double price = Zomato.search(foodName);
+        System.out.println("The price of " + foodName + " is: " + price);
+    }
+}
+
+
